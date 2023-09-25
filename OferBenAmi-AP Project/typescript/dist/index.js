22482654f792cc31c49ba144035d7fd4e9a7f47e
OferBenA <132418948+OferBenA@users.noreply.github.com>
2023-09-25 05:21:28 +0300

    Starting ts

---
+++ OferBenAmi-AP Project/typescript/dist/index.js
@@ -0,0 +1,5 @@
+var apTable = document.querySelector(".apTable");
+var form = document.querySelector(".Form");
+function submitForm(event) {
+    event.preventDefault();
+}
---
+++ OferBenAmi-AP Project/typescript/index.ts
@@ -0,0 +1,7 @@
+const apTable = document.querySelector(".apTable");
+const form = document.querySelector(".Form");
+
+function submitForm(event: SubmitEvent){
+  event.preventDefault();
+
+}
