36492cc13d062d1affc6a0053402c1ae06ea74de
ofer ben ami <benamiofer1@gmail.com>
2023-10-01 04:54:16 +0300

    starting the script

--- OferBenAmi-AP Project/typescript/dist/index.js
+++ OferBenAmi-AP Project/typescript/dist/index.js
@@ -1,5 +1,27 @@
-var apTable = document.querySelector(".apTable");
-var form = document.querySelector(".Form");
+var apDetailsEntry = /** @class */ (function () {
+    function apDetailsEntry(vendorName, modelName, hostName, macAddr, ipAddr, Version) {
+        this.vendorName = vendorName;
+        this.modelName = modelName;
+        this.hostName = hostName;
+        this.macAddr = macAddr;
+        this.ipAddr = ipAddr;
+        this.Version = Version;
+    }
+    return apDetailsEntry;
+}());
+var jsonCounter = 0;
 function submitForm(event) {
     event.preventDefault();
+    var form = event.target;
+    var elements = form.elements;
+    var newEntry = {
+        vendorName: elements.vendorName.value,
+        modelName: elements.modelName.value,
+        hostName: elements.hostName.value,
+        macAddr: elements.macAddr.value,
+        ipAddr: elements.ipAddr.value,
+        Version: elements.Version.value
+    };
+    localStorage.setItem("itemNum" + jsonCounter++, JSON.stringify(newEntry));
+    console.log(newEntry);
 }
--- OferBenAmi-AP Project/typescript/index.ts
+++ OferBenAmi-AP Project/typescript/index.ts
@@ -1,7 +1,37 @@
-const apTable = document.querySelector(".apTable");
-const form = document.querySelector(".Form");
+class apDetailsEntry {
+	constructor(
+		public vendorName: string,
+		public modelName: string,
+		public hostName: string,
+		public macAddr: string,
+		public ipAddr: string,
+		public Version: string,
+	) { }
+}
+let jsonCounter:number = 0;
+interface formExtention extends HTMLFormControlsCollection{
+	vendorName: HTMLInputElement;
+	modelName: HTMLInputElement;
+	hostName: HTMLInputElement;
+	macAddr: HTMLInputElement;
+	ipAddr: HTMLInputElement;
+	Version: HTMLInputElement;
+}
 
-function submitForm(event: SubmitEvent){
-  event.preventDefault();
 
+function submitForm(event: SubmitEvent) {
+	event.preventDefault();
+	const form = event.target as HTMLFormElement;
+	const elements = form.elements as formExtention;
+
+	let newEntry: apDetailsEntry = {
+		vendorName: elements.vendorName.value,
+		modelName: elements.modelName.value,
+		hostName: elements.hostName.value,
+		macAddr: elements.macAddr.value,
+		ipAddr: elements.ipAddr.value,
+		Version: elements.Version.value,
+	};
+	localStorage.setItem(`itemNum${jsonCounter++}`,JSON.stringify(newEntry))
+	console.log(newEntry)
 }
